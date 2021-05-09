a2bb8c8708b181556893237fa8260c42a5c83005
Carlos Ladino <carlos.e.ladino@nasa.gov>
2021-05-08 21:24:44 -0400

    Fixing Avartar

--- DerbyGame/ClientApp/src/app/common/CustomComponents/guest.component.ts
+++ DerbyGame/ClientApp/src/app/common/CustomComponents/guest.component.ts
@@ -14,7 +14,11 @@ export class GuestComponent {
 
     @Input() avatarName: string;
     @Input() guestName: string;
+    @Input() size: string = "small"; 
     public avatarFileName: string;
+  public imageSize: string = '';
+  public fontSize: string = '';
+
     constructor() {
       
     }
@@ -25,7 +29,14 @@ export class GuestComponent {
         }
         else {
             this.avatarFileName = this.avatarName;
-        }
+      }
+      if (this.size == "small") {
+        this.imageSize = "30px";
+        this.fontSize = "11px";
+      } else {
+        this.imageSize = "90px";
+        this.fontSize = "40px";
+      }
     }
 
 }
